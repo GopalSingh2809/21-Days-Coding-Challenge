56c376c49c3a8495b9d0c15b67a9ed27ba95acd5
Gopal <gss428860@gmail.com>
2025-03-30 13:56:54 +0530

    Day 11 challenge Completed

---
+++ MedianFromData.py
@@ -0,0 +1,28 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.max_heap = []  # stores the lower half (max heap using min heap with inverted values)
+        self.min_heap = []  # stores the upper half (min heap)
+
+    def addNum(self, num: int) -> None:
+        if not self.max_heap or num <= -self.max_heap[0]:
+            heapq.heappush(self.max_heap, -num)
+        else:
+            heapq.heappush(self.min_heap, num)
+        
+        # Balance the heaps
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            val = -heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, val)
+        elif len(self.min_heap) > len(self.max_heap):
+            val = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -val)
+
+    def findMedian(self) -> float:
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        else:
+            return -self.max_heap[0]
+        
